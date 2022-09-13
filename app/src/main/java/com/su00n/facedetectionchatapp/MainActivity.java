3e3c29775c95f1fde75e4d36cc35a991af6ea07f
su00n <he.su00n@gmail.com>
2022-09-13 12:53:36 +0530

    TEXT ADDED

--- app/src/main/java/com/su00n/facedetectionchatapp/MainActivity.java
+++ app/src/main/java/com/su00n/facedetectionchatapp/MainActivity.java
@@ -3,9 +3,10 @@ package com.su00n.facedetectionchatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+  TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
